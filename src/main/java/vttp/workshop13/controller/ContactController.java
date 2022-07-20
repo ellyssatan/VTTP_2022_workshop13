551a0d67c53b57bfa692e10fe782bc513680dc1d
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-20 16:46:42 +0800

    removed unused imports

--- src/main/java/vttp/workshop13/controller/ContactController.java
+++ src/main/java/vttp/workshop13/controller/ContactController.java
@@ -1,8 +1,5 @@
 package vttp.workshop13.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import vttp.workshop13.model.Contact;
 import vttp.workshop13.services.DatabaseService;
